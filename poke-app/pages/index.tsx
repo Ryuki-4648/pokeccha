ba440a765ee850a8204ec68579229a0a9b0ae079
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2023-04-12 08:04:43 +0900

    一覧ページにclass追加

--- poke-app/pages/index.tsx
+++ poke-app/pages/index.tsx
@@ -16,7 +16,7 @@ const PokemonList: NextPage = () => {
 
   useEffect(() => {
     const fetchPokemon = async () => { // asyncを書いて非同期（async）関数であることを宣言
-      const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10"); // PokeAPI取得
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=30"); // PokeAPI取得
       const data = await response.json();
 
       const results = await Promise.all(
@@ -30,7 +30,7 @@ const PokemonList: NextPage = () => {
             id: pokemonData.id, 
             name: pokemonData.name,
             image: pokemonData.sprites.other['official-artwork'].front_default,
-            types: pokemonData.types,
+            types: pokemonData.types[0].type.name,
             height: pokemonData.height,
             weight: pokemonData.weight,
           };
@@ -45,15 +45,17 @@ const PokemonList: NextPage = () => {
   }, []);
 
   return (
-    <div>
-      <h1 className="text-center text-4xl">pokeccha</h1>
-      <ul>
+    <div className="px-10 pt-20">
+      <h1 className="text-center text-2xl mb-3 tracking-wider">pokeccha</h1>
+      <h2 className="text-center text-7xl mb-28 uppercase tracking-wider">Menu</h2>
+      <ul className="flex flex-wrap justify-between">
         {pokemonList.map((pokemon) => (
-        <li key={pokemon.id}>
-          <p>No. {pokemon.id}</p>
-          <p>{pokemon.name}</p>
+        <li key={pokemon.id} className="mb-24 my-auto px-20 w-1/4">
           <img src={pokemon.image} alt={`${pokemon.name} Image`} />
           {/* <Image src={pokemon.image} alt={`${pokemon.name} Image`} width={100} height={100} /> */}
+          <p className="mb-2">Order No. {pokemon.id}</p>
+          <p className="text-2xl uppercase tracking-wider">{pokemon.name}</p>
+          <p>Type: {pokemon.types}</p>
         </li>
       ))}
       </ul>
