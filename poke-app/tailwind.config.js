1950cc485c1732e109e1eeede178a6654c2f56c9
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2023-04-12 00:04:32 +0900

    yarn.lockとconfigファイル修正

--- poke-app/tailwind.config.js
+++ poke-app/tailwind.config.js
@@ -1,8 +1,23 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  mode: "jit", // Just -in-Time Mode 必要に応じてスタイルを反映するモード
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'teal': colors.teal,
+      },
+    },
   },
-  plugins: [],
+  purge: [ // ビルド生成時の未使用のスタイルを除外し、パフォーマンスを最適化
+    // "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  plugins: [
+    // `@tailwindcss/aspect-ratio`を有効にする
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
