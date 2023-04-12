97a3a197f861f59cbd1a95669677d5b82bb91cf1
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2023-04-13 08:47:24 +0900

    ポケモン一覧のスタイル調整

--- poke-app/pages/index.tsx
+++ poke-app/pages/index.tsx
@@ -1,16 +1,16 @@
 import { NextPage } from 'next';
 import { useEffect, useState } from 'react'
 
-// const inter = Inter({ subsets: ['latin'] })
-
 interface Pokemon {
   id: number;
   name: string;
   image : string;
-  types: string;
+  types01: string;
+  //types02: string;
   height: number;
   weight: number;
 }
+
 const PokemonList: NextPage = () => {
   const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
 
@@ -24,13 +24,14 @@ const PokemonList: NextPage = () => {
           const res = await fetch(pokemon.url);
           const pokemonData = await res.json();
 
-          const pokemonSpecies = await fetch(pokemonData.species.url);
+          //const pokemonSpecies = await fetch(pokemonData.species.url);
           // console.log(data);
           return { 
             id: pokemonData.id, 
             name: pokemonData.name,
             image: pokemonData.sprites.other['official-artwork'].front_default,
-            types: pokemonData.types[0].type.name,
+            types01: pokemonData.types[0].type.name,
+            //types02: pokemonData.types[1].type.name,
             height: pokemonData.height,
             weight: pokemonData.weight,
           };
@@ -45,21 +46,49 @@ const PokemonList: NextPage = () => {
   }, []);
 
   return (
-    <div className="px-10 pt-20">
-      <h1 className="text-center text-2xl mb-3 tracking-wider">pokeccha</h1>
-      <h2 className="text-center text-7xl mb-28 uppercase tracking-wider">Menu</h2>
-      <ul className="flex flex-wrap justify-between">
-        {pokemonList.map((pokemon) => (
-        <li key={pokemon.id} className="mb-24 my-auto px-20 w-1/4">
-          <img src={pokemon.image} alt={`${pokemon.name} Image`} />
-          {/* <Image src={pokemon.image} alt={`${pokemon.name} Image`} width={100} height={100} /> */}
-          <p className="mb-2">Order No. {pokemon.id}</p>
-          <p className="text-2xl uppercase tracking-wider">{pokemon.name}</p>
-          <p>Type: {pokemon.types}</p>
-        </li>
-      ))}
-      </ul>
-    </div>
+    <>
+      <div className="flex justify-between pt-10 pl-10">
+        <div className="flex items-center">
+          <h1 className="text-center text-4xl tracking-wider mr-6">pokeccha</h1>
+          <h2 className="text-center text-1xl tracking-wider leading-loose">Pick your favorite Pokemon!</h2>
+        </div>
+        <div className="flex mr-10 text-sm">
+          <div className="mr-6 tracking-wide">
+            <p className="mb-1">Mon - Fri : 9AM - 11PM</p>
+            <p className="">Sat - Sun : 8AM - 20PM</p>
+          </div>
+          <div className="tracking-wider">
+            <p className="mb-1">The RESTful Pokémon API</p>
+            <p>https://pokeapi.co/</p>
+          </div>
+          
+        </div>
+      </div>
+      <div className="px-10 pt-24 flex justify-between">
+        <div className="fixed w-1/12">
+          <p className="text-xl uppercase tracking-wider">types</p>
+          <ul>
+            <li>grass</li>
+            <li>fire</li>
+            <li>water</li>
+            <li>bug</li>
+            <li>normal</li>
+          </ul>
+          <p>etc</p>
+        </div>
+        <ul className="flex flex-wrap justify-between w-11/12 ml-auto">
+          {pokemonList.map((pokemon) => (
+          <li key={pokemon.id} className="mb-24 my-auto px-16 w-1/4">
+            <img src={pokemon.image} alt={`${pokemon.name} Image`} />
+            {/* <Image src={pokemon.image} alt={`${pokemon.name} Image`} width={100} height={100} /> */}
+            <p className="mb-2">Order No. {pokemon.id}</p>
+            <p className="text-2xl uppercase tracking-wider font-bold">{pokemon.name}</p>
+            <p>Type: {pokemon.types01}</p>
+          </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
 
